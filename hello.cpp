504bee77bf34cf43f6e17676ba2abefeebffccfc
tetilabskill2025 <tugmat.tls25@gmail.com>
2025-08-24 14:43:36 +0700

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, i just landed to the Git Playground!" << endl;
+    cout << "Edit this file to print your own name or a fun message for your friends (or tugmat) for everyone to see :)" << endl;
+
+    // Example: Uncomment and change this line
+    // cout << "Haii, this is Tugmat speaking!" << endl;
+
+    return 0;
+}
